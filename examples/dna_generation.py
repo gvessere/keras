816172d14c2c3db5896f24f6e72a0cb240d7faae
Gery Vessere <gvessere@illumina.com>
2016-02-21 10:53:09 -0800

    dna model wip

--- examples/dna_generation.py
+++ examples/dna_generation.py
@@ -1,40 +1,28 @@
-'''Example script to generate text from Nietzsche's writings.
-
-At least 20 epochs are required before the generated text
-starts sounding coherent.
-
-It is recommended to run this script on GPU, as recurrent
-networks are quite computationally intensive.
-
-If you try this script on new data, make sure your corpus
-has at least ~100k characters. ~1M is better.
-'''
+# Generate from sequencing data
 
 from __future__ import print_function
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout, Masking
 from keras.layers.recurrent import LSTM
-from keras.datasets.data_utils import get_file
+from keras.datasets.data_utils import get_file, Progbar
 import matplotlib.pyplot as plt
 import numpy as np
 import random
 import sys
 
-# file with the following commands 
-# wget -O dna "http://genome.ucsc.edu/cgi-bin/hgTables?hgsid=476317005_ucqyJHBYfqmTY6NouE8av4h0jaDj&hgSeq.promoter=on&boolshad.hgSeq.promoter=0&hgSeq.promoterSize=1000&hgSeq.utrExon5=on&boolshad.hgSeq.utrExon5=0&hgSeq.cdsExon=on&boolshad.hgSeq.cdsExon=0&hgSeq.utrExon3=on&boolshad.hgSeq.utrExon3=0&hgSeq.intron=on&boolshad.hgSeq.intron=0&hgSeq.downstream=on&boolshad.hgSeq.downstream=0&hgSeq.downstreamSize=1000&hgSeq.granularity=gene&hgSeq.padding5=0&hgSeq.padding3=0&boolshad.hgSeq.splitCDSUTR=0&hgSeq.casing=exon&boolshad.hgSeq.maskRepeats=0&hgSeq.repMasking=lower&hgta_doGenomicDna=get+sequence" 
-# cat dna | perl -pe "s/^(>.+)$/\1|/g" |perl -pe "s/\|/\t/g" | perl -pe "s/=([-+])/\t\1\t/" | perl -pe "s/chr(.+?):(\d+)-(\d+)/\t\1\t\2\t\3\t/" | cut -f1,2,3,4,6,8 | perl -pe "s/>.+?\t/|/s" | perl -pe "s/\n//s" | tr "|" "\n" > kg.fasta.formatted.seq.sample
+# input sequence file obtained with the following commands 
+# returns genes with uppercased exons (no 5' and 3' UTRs)
+# wget "http://genome.ucsc.edu/cgi-bin/hgTables?hgsid=476467603_ZpmULv360T1UveJRDFvf1WjGEDq6&hgSeq.promoter=on&boolshad.hgSeq.promoter=0&hgSeq.promoterSize=300&hgSeq.utrExon5=on&boolshad.hgSeq.utrExon5=0&hgSeq.cdsExon=on&boolshad.hgSeq.cdsExon=0&hgSeq.utrExon3=on&boolshad.hgSeq.utrExon3=0&hgSeq.intron=on&boolshad.hgSeq.intron=0&hgSeq.downstream=on&boolshad.hgSeq.downstream=0&hgSeq.downstreamSize=300&hgSeq.granularity=feature&hgSeq.padding5=50&hgSeq.padding3=50&hgSeq.splitCDSUTR=on&boolshad.hgSeq.splitCDSUTR=0&hgSeq.casing=exon&boolshad.hgSeq.maskRepeats=0&hgSeq.repMasking=lower&hgta_doGenomicDna=get+sequence" -O kg_one_record_per_region.fasta
+# cat kg_one_record_per_region.fasta | perl -pe "s/^(>.+)$/\1|/g" |perl -pe "s/\|/\t/g" | perl -pe "s/=([-+])/\t\1\t/" | perl -pe "s/chr(.+?):(\d+)-(\d+)/\t\1\t\2\t\3\t/" | cut -f1,2,3,4,6,8 | perl -pe "s/>.+?\t/|/s" | perl -pe "s/\n//s" | tr "|" "\n" | cut -f5 | sort | uniq > ~/.keras/datasets/kg_one_record_per_region.seq.uq
 
-
-path = get_file('kg.fasta.formatted.seq.sample', origin="")
+path = get_file('kg_one_record_per_region.seq.uq', origin="")
 
 text = open(path).read()
 print('corpus length:', len(text))
 genes=text.split('\n')
 print('total sequences:', len(genes))
 
-for i in range(len(genes)):
-    genes[i]=genes[i] + '.' 
-chars = set(genes[1])
+chars = set(genes[2])
 print('total chars:', len(chars))
 char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
@@ -42,7 +30,7 @@ indices_char = dict((i, c) for i, c in enumerate(chars))
 # vectorize genes of size at most maxlen
 maxlen = 800 
 step = 1
-batchsize=32
+batchsize=256
 dropout=0.2
 
 if len(sys.argv) > 1:
@@ -54,23 +42,23 @@ def sample(a, temperature=1.0):
     a = np.exp(a) / np.sum(np.exp(a))
     return np.argmax(np.random.multinomial(1, a, 1))
 
-def getNNData(sentences, minLength, maxLength):
+def getNNData(genes, minLength, maxLength):
     sentences = []
-    next_chars = []
     for i in range(len(genes)):
-        if (len(genes[i]) < maxlen) and (len(genes[i]) < maxlen - 200):
+        l=len(genes[i])
+        if minLength <= l and l < maxLength:
             sentences.append(genes[i][:-1])
-            next_chars.append(genes[1:i])
         
-    print('nb sequences of length at most ' + str(maxlen) + ':', len(sentences))
+    print('nb sequences in range(' + str(minLength) + ',' + str(maxLength) + ') : ', len(sentences))
     #print(sentences)
     print('Vectorization...')
-    X = np.zeros((samples, maxlen, len(chars)), dtype=np.bool)
-    y = np.zeros((samples, maxlen, len(chars)), dtype=np.bool)
+    X = np.zeros((len(sentences), maxLength, len(chars)), dtype=np.bool)
     for i, sentence in enumerate(sentences):
         for t, char in enumerate(sentence):
             X[i, t, char_indices[char]] = 1
-            
+    
+    y=np.zeros(X.shape)
+    y[:,:-1,:]=X[:,1:,:]
     return (X,y)
 
 def saveModel(model):
@@ -78,7 +66,35 @@ def saveModel(model):
     open('dnaModel2.json', 'w').write(json_string)
     model.save_weights('dnaModel2.mod', overwrite=True)    
 
-# build the model: 3 stacked LSTM
+def createBatch(batchId, batchsize, X,y):
+    slicemin = batchsize*batchId
+    slicemax = min(batchsize*(batchId+1), X.shape[0])
+    sliceSize = slicemax-slicemin
+    batchSamples=slice(slicemin, slicemax)
+    Xbatch=np.zeros((sliceSize,1,len(chars)), dtype=np.bool)
+    ybatch=np.zeros((sliceSize,len(chars)), dtype=np.bool)
+    
+    Xbatch[:sliceSize,:,:]=np.reshape(X[batchSamples,timestep,:], (sliceSize, 1, len(chars)))
+    ybatch[:sliceSize,:]=y[batchSamples,timestep,:]
+    return (Xbatch,ybatch)
+    
+
+def doValidation():
+    model.save_weights('validation.mod', overwrite=True)
+    model.reset_states()
+
+    for batch in range(len(Xval)/batchsize):
+        for timestep in range(windowEnd):
+            Xbatch,ybatch=createBatch(batch, batchsize, Xval, yval)
+            nonmasked = np.sum(Xbatch)
+            
+            if nonmasked > 0:
+                [ loss, accuracy ] = model.train_on_batch(Xbatch,ybatch, accuracy=True)
+                normloss = losses[-1][0].tolist / nonmasked
+                print("val loss: %0.3f accuracy: %0.3f, normalized loss: %0.3f" % (loss, accuracy, normloss))
+            
+    model.load_weights('validation.mod') 
+
 print('Build model...')
 model = Sequential()
 model.add(Masking(mask_value=0, batch_input_shape=[batchsize,1,len(chars)]))
@@ -91,37 +107,45 @@ model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-samples=len(sentences)
-trainingSamples=samples*0.9
-validationSamples=samples*0.1
+samples=len(genes)
+trainingSamples=int(samples*0.8)
+validationSamples=int(samples*0.2)
 
 # train on data ranges of increasingly long sequences
 # eg. 0-200, 200-400 etc...
 windowSize = 200
-for windowStart in range(0,1000,windowSize):
-    (Xtrain,ytrain)=getNNData(sentences[:trainingSamples], windowStart,windowStart + windowSize)
-    (Xval,yval)=getNNData(sentences[-validationSamples:], windowStart,windowStart + windowSize)
 
     # train the model, output generated text after each batch
-    losses = []
-    for epoch in range(60):
-        print("epoch: %d" % epoch)
-        for batch in range((samples-1)/batchsize):
+losses = []
+for epoch in range(60):
+    print("")
+    print("epoch: %d" % epoch)
+    for windowStart in range(0,1600,windowSize):
+        windowEnd=windowStart+windowSize
+        print("")
+        print("processing sequences in window %d-%d" % (windowStart, windowEnd))
+        (Xtrain,ytrain)=getNNData(genes[:trainingSamples], windowStart,windowEnd)
+        (Xval,yval)=getNNData(genes[-validationSamples:], windowStart,windowEnd)
+        if len(Xtrain) == 0:
+            continue
+            
+        for batch in range(len(Xtrain)/batchsize):
             print()
             print('-' * 50)
-            for timestep in range(maxlen):
-                batchSamples=slice(batchsize*batch,batchsize*(batch+1))
-                Xbatch=np.reshape(X[batchSamples,timestep,:], (batchsize, 1, len(chars)))
-                ybatch=y[batchSamples,timestep,:]
-                loss=model.train_on_batch(Xbatch,ybatch)
-                losses.append(loss[0].tolist())
-                print("batch %d, timestep %d, loss: %0.3f" % (batch, timestep, losses[-1]), end='\r')
-                if timestep == 1:
-                    print()
-                sys.stdout.flush()
-                
             model.reset_states()
-        saveModel(model)
+            progbar=Progbar(windowEnd)
+            for timestep in range(windowEnd):
+                Xbatch,ybatch=createBatch(batch, batchsize, Xtrain, ytrain)
+                
+                nonmasked = np.sum(Xbatch)
+                if (nonmasked > 0):
+                    loss=model.train_on_batch(Xbatch,ybatch)
+                    losses.append(loss[0].tolist())
+                    normloss = losses[-1] / nonmasked
+                    progbar.update(1+timestep, values=[('train loss', loss[0].tolist()), ('timestep', timestep), ('sum(nonmasked)', nonmasked), ('normlossx1000', 1000*normloss)])
+
+    doValidation()
+    saveModel(model)
 
     # plt.title('training loss')
     # plt.plot(losses)
