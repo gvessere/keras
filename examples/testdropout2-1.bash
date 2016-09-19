1cf7fbf464eff62445b0365c32a81227da55e829
Gery Vessere <gery@vessere.com>
2016-09-19 02:41:40 +0000

    update file

--- examples/testdropout2-1.bash
+++ examples/testdropout2-1.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-THEANO_FLAGS=device=gpu0 python cifar10_cnn.py 0.25 0.45 0.5 0.7 > dropout2_0.25_0.45_0.5_0.7.log
-THEANO_FLAGS=device=gpu0 python cifar10_cnn.py 0.25 0.25 0.5 0.5 > dropout2_0.25_0.25_0.5_0.5.log
+#THEANO_FLAGS=device=gpu0 python cifar10_cnn.py 0.25 0.45 0.5 0.7 > dropout2_0.25_0.45_0.5_0.7.log # ok
+THEANO_FLAGS=device=gpu0 python cifar10_cnn.py 0.25 0.25 0.5 0.5 > dropout2_0.25_0.25_0.5_0.5.log # ?
 THEANO_FLAGS=device=gpu0 python cifar10_cnn.py 0.3 0.3 0.55 0.55 > dropout2_0.3_0.3_0.55_0.55.log
 THEANO_FLAGS=device=gpu0 python cifar10_cnn.py 0.35 0.35 0.6 0.6 > dropout2_0.35_0.35_0.6_0.6.log
 
--- examples/testdropout2-4.bash
+++ examples/testdropout2-4.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 
-
-THEANO_FLAGS=device=gpu3 python cifar10_cnn.py 0.25 0.25 0.55 0.55 > dropout2_0.25_0.25_0.55_0.55.log
-THEANO_FLAGS=device=gpu3 python cifar10_cnn.py 0.25 0.25 0.6 0.6 > dropout2_0.25_0.25_0.6_0.6.log
+#THEANO_FLAGS=device=gpu3 python cifar10_cnn.py 0.25 0.25 0.55 0.55 > dropout2_0.25_0.25_0.55_0.55.log # 0.1
+#THEANO_FLAGS=device=gpu3 python cifar10_cnn.py 0.25 0.25 0.6 0.6 > dropout2_0.25_0.25_0.6_0.6.log # 0.1
 THEANO_FLAGS=device=gpu3 python cifar10_cnn.py 0.25 0.25 0.65 0.65 > dropout2_0.25_0.25_0.65_0.65.log
 THEANO_FLAGS=device=gpu3 python cifar10_cnn.py 0.25 0.25 0.7 0.7 > dropout2_0.25_0.25_0.7_0.7.log
